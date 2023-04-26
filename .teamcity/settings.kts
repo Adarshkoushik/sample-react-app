edc8e7f84f6118110c274b9f293757cb0e353d18
saichandanal <saichandana2021@gmail.com>
2023-04-26 14:17:48 +0530

    add 1 script

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -36,6 +37,13 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name= "Install npm"
+            scriptContent= """ npm install"""
+        }
     }
 
     triggers {
